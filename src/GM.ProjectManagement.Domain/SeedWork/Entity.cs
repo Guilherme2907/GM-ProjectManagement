641b9dd799e357630f435b663309bcf63bf509b0
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2024-12-16 20:13:15 -0300

    Creation of a seedwork "Entity" for every entity extends from

---
+++ src/GM.ProjectManagement.Domain/SeedWork/Entity.cs
@@ -0,0 +1,8 @@
+﻿namespace GM.ProjectManagement.Domain.SeedWork;
+
+public abstract class Entity
+{
+    protected Guid Id { get; set; } = Guid.NewGuid();
+    public DateTime CreatedAt { get; private set; } = DateTime.Now;
+    public DateTime UpdatedAt { get; private set; } = DateTime.Now;
+}
