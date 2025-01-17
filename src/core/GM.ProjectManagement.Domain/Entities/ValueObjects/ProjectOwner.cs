060ad8e5d8711d61c8b454ecc1ed465982c21f3b
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2025-01-16 22:22:20 -0300

    feat(domain): add ProjectOwner value object to manage owners

---
+++ src/core/GM.ProjectManagement.Domain/Entities/ValueObjects/ProjectOwner.cs
@@ -0,0 +1,25 @@
+﻿using GM.ProjectManagement.Domain.Enums;
+using GM.ProjectManagement.Domain.Exceptions;
+using GM.ProjectManagement.Domain.Validations;
+
+namespace GM.ProjectManagement.Domain.Entities.ValueObjects;
+
+public class ProjectOwner
+{
+    public ProjectMember Owner { get; private set; }
+
+    public ProjectOwner(ProjectMember owner)
+    {
+        DomainValidation.NotNull(owner, nameof(Owner));
+
+        if (!IsAValidProjectOwner(owner))
+            throw new EntityValidationException($"Only admins and managers can be project owners");
+
+        Owner = owner;
+    }
+
+    private static bool IsAValidProjectOwner(ProjectMember owner)
+    {
+        return owner.Role == ProjectMemberRole.Manager || owner.Role == ProjectMemberRole.Admin;
+    }
+}
