373726040433f185df8a874d648468ade7a2b171
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2025-01-27 17:50:11 -0300

    Changed name from Collaborator to ProjectMember

---
+++ tests/GM.ProjectManagement.UnitTests/Domain/Entities/ProjectMember/ProjectMemberTestFixture.cs
@@ -0,0 +1,11 @@
+﻿using GM.ProjectManagement.UnitTests.Common;
+
+namespace GM.ProjectManagement.UnitTests.Domain.Entities.ProjectMember;
+
+[CollectionDefinition(nameof(ProjectMemberTestFixture))]
+public class ProjectMemberTestFixtureCollection : ICollectionFixture<ProjectMemberTestFixture>
+{ }
+
+public class ProjectMemberTestFixture : BaseFixture
+{
+}
