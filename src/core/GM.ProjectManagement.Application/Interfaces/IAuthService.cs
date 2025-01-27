0b88d100f734672b6587cfd0ad8ab1770cd41ad9
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2025-01-27 17:57:55 -0300

    Creation of service interfaces

---
+++ src/core/GM.ProjectManagement.Application/Interfaces/IAuthService.cs
@@ -0,0 +1,8 @@
+﻿using GM.ProjectManagement.Application.UseCases.LoginUser;
+
+namespace GM.ProjectManagement.Application.Interfaces;
+
+public interface IAuthService
+{
+    Task<LoginUserOutput> LoginUserAsync(LoginUserInput input, CancellationToken cancellationToken);
+}
---
+++ src/core/GM.ProjectManagement.Application/Interfaces/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using GM.ProjectManagement.Application.UseCases.CreateUser;
+using GM.ProjectManagement.Application.UseCases.LoginUser;
+
+namespace GM.ProjectManagement.Application.Interfaces;
+
+public interface IUserService
+{
+    Task CreateAsync(CreateUserInput input, CancellationToken cancellationToken);
+}
